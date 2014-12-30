d440312a737b2200b32c75dee892156c68bdbbce
Matthew Andrews <matt@mattandre.ws>
2014-12-30 15:32:27 +0000

    Debug output

--- tasks/clean.js
+++ tasks/clean.js
@@ -2,5 +2,8 @@ var denodeify = require('denodeify');
 var exec = denodeify(require('child_process').exec, function(err, stdout, stderr) { return [err, stdout]; });
 
 module.exports = function() {
-	return exec('git clean -fxd');
+	return exec('git clean -fxd')
+		.then(function(output) {
+			console.log(output);
+		});
 };
