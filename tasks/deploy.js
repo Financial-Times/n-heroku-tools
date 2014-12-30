b55abea633e6fbeab9715cfea55258d98efe3432
Matthew Andrews <matt@mattandre.ws>
2014-12-30 12:58:59 +0000

    Fix requires

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,7 +1,8 @@
-var haikro = require('haikro');
 var packageJson = require(process.cwd() + '/package.json');
 var denodeify = require('denodeify');
 var exec = denodeify(require('child_process').exec, function(err, stdout, stderr) { return [err, stdout]; });
+var build = require('haikro/lib/build');
+var deploy = require('haikro/lib/deploy');
 
 function normalizeName(name) {
 	var matches = name.match(/^(?:ft-)?(?:next-)?(.*)/);
