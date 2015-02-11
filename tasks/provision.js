1e47f1e6f0dc4172c1f26666f9721ca9a28a3080
Matt Chadburn <matt.chadburn@ft.com>
2015-02-11 14:53:11 +0000

    Remove branch name from the app

--- tasks/provision.js
+++ tasks/provision.js
@@ -19,11 +19,10 @@ function normalizeName(name) {
 module.exports = function () {
 
     var build = process.env.CI_BUILD_NUMBER;
-    var branch = process.env.CI_BRANCH;
-    var heroku_auth = process.env.HEROKU_API_TOKEN;
+    var heroku_auth = process.env.HEROKU_AUTH_TOKEN;
 
     if (!branch || !build || !heroku_auth) {
-        throw "You need to set HEROKU_API_TOKEN, CI_BRANCH, and CI_BUILD_NUMBER environment variables";
+        throw "You need to set HEROKU_AUTH_TOKEN and CI_BUILD_NUMBER environment variables";
     }
 
 	var token;
@@ -35,7 +34,7 @@ module.exports = function () {
 			token = results[0].trim();
 			var project = 'ft-next-' + normalizeName(packageJson.name);
             var server = {
-                app: util.format('%s-%s-%s', project, branch, build),
+                app: util.format('%s-%s', project, build),
                 region: 'eu',
 				token: token,
                 organization: 'financial-times'
