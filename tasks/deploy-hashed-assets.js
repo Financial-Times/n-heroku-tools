15d6012556af01c6787a3933c70a5c90fd48f95c
Matthew Andrews <matt@mattandre.ws>
2015-03-20 10:24:50 +0000

    Upload hashed files one at a time to work around GitHub issue :(

--- tasks/deploy-hashed-assets.js
+++ tasks/deploy-hashed-assets.js
@@ -9,19 +9,59 @@ var glob = denodeify(require('glob'));
 var crypto = require('crypto');
 var basename = require('path').basename;
 
-module.exports = function(app) {
+function hashAndUpload(opts) {
+	var file = opts.file;
+	var app = opts.app;
 	var token = process.env.GITHUB_AUTH_TOKEN;
-	if (!token) {
-		return Promise.reject("GITHUB_AUTH_TOKEN must be set");
-	}
-
-	app = app || normalizeName(packageJson.name, { version: false });
 	var authorizedHeaders = {
 		'Content-Type': 'application/json',
 		'Accept': 'application/vnd.github.v3+json',
 		'Authorization': 'token ' + token
 	};
 	var api = 'https://api.github.com/repos/Financial-Times/next-hashed-assets/contents/';
+	var hash = crypto.createHash('sha1').update(file.content.toString('utf8')).digest('hex');
+	var hashedName = file.name.replace(/(.*)(\.[a-z0-9])/i, '$1-' + hash.substring(0, 8) + '$2');
+	return fetch(api + encodeURIComponent(app + '/' + hashedName), {
+		method: 'PUT',
+		headers: authorizedHeaders,
+		body: JSON.stringify({
+			message: 'Create ' + file.name + ' for ' + app,
+			content: file.content.toString('base64'),
+			branch: 'gh-pages',
+			committer: {
+				name: 'Next Team',
+				email: 'next.team@ft.com'
+			}
+		})
+	})
+		.then(function(response) {
+			if (response.status === 201) {
+				console.log('Successfully pushed ' + file.name + ' to GitHub for app ' + app);
+			} else {
+				return response.json()
+					.then(function(err) {
+						if (err.message === 'Invalid request.\n\n"sha" wasn\'t supplied.') {
+							console.log('Hashed file ' + app + '/' + hashedName + ' already exists');
+						} else {
+							throw new Error(err.message);
+						}
+					});
+			}
+		})
+		.then(function() {
+			return {
+				name: file.name,
+				hashedName: hashedName
+			};
+		});
+
+}
+
+module.exports = function(app) {
+	if (!process.env.GITHUB_AUTH_TOKEN) {
+		return Promise.reject("GITHUB_AUTH_TOKEN must be set");
+	}
+	app = app || normalizeName(packageJson.name, { version: false });
 
 	return glob(process.cwd() + '/public/*.@(css|js|map)')
 		.then(function(files) {
@@ -36,51 +76,19 @@ module.exports = function(app) {
 			}));
 		})
 		.then(function(files) {
-			return Promise.all(files.map(function(file) {
-				// PUT /repos/:owner/:repo/contents/:path
-				var hash = crypto.createHash('sha1').update(file.content.toString('utf8')).digest('hex');
-				var hashedName = file.name.replace(/(.*)(\.[a-z0-9])/i, '$1-' + hash.substring(0, 8) + '$2');
-				return fetch(api + encodeURIComponent(app + '/' + hashedName), {
-					method: 'PUT',
-					headers: authorizedHeaders,
-					body: JSON.stringify({
-						message: 'Create ' + file.name + ' for ' + app,
-						content: file.content.toString('base64'),
-						branch: 'gh-pages',
-						committer: {
-							name: 'Next Team',
-							email: 'next.team@ft.com'
-						}
-					})
-				})
-					.then(function(response) {
-						if (response.status === 201) {
-							console.log('Successfully pushed ' + file.name + ' to GitHub for app ' + app);
-						} else {
-							return response.json()
-								.then(function(err) {
-									if (err.message === 'Invalid request.\n\n"sha" wasn\'t supplied.') {
-										console.log('Hashed file ' + app + '/' + hashedName + ' already exists');
-									} else {
-										throw new Error(err.message);
-									}
-								});
-						}
-					})
-					.then(function() {
-						return {
-							name: file.name,
-							hashedName: hashedName
-						};
-					});
-			}));
+			var promise = files.reduce(function(promise, file) {
+					return promise.then(function(obj) {
+						return hashAndUpload({ app: app, file: file })
+							.then(function(file) {
+								obj[file.name] = file.hashedName;
+								return obj;
+							});
+						});
+				}, Promise.resolve({}));
+			return promise;
 		})
 		.then(function(hashes) {
-			var hashMap = hashes.reduce(function(obj, file) {
-				obj[file.name] = file.hashedName;
-				return obj;
-			}, {});
 			console.log("Writing public/asset-hashes.json");
-			return writeFile(process.cwd() + '/public/asset-hashes.json', JSON.stringify(hashMap, undefined, 2));
+			return writeFile(process.cwd() + '/public/asset-hashes.json', JSON.stringify(hashes, undefined, 2));
 		});
 };
