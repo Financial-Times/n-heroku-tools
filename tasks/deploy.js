5f4a67c41a3f52899c0499281739dadd50ebca27
Matthew Andrews <matt@mattandre.ws>
2015-04-28 13:05:31 +0100

    Missing semi-color

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -24,7 +24,7 @@ module.exports = function(app) {
 		.then(function(results) {
 			token = results[0];
 			commit = results[1].trim();
-			return about({ name: name, commit: commit })
+			return about({ name: name, commit: commit });
 		})
 		.then(function() {
 			return Promise.all([
