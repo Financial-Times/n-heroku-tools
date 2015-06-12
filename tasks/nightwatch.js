8488f81285834e071a0765f2070b7278b1d546a2
Matthew Andrews <matt@mattandre.ws>
2015-06-12 15:36:58 +0100

    Spawn nightwatch

--- tasks/nightwatch.js
+++ tasks/nightwatch.js
@@ -1,19 +1,31 @@
 'use strict';
 
 var denodeify = require('denodeify');
-var exec = denodeify(require('child_process').exec, function(err, stdout, stderr) {
-	console.log(stdout);
-	console.log(stderr);
-	return [err];
-});
 var path = require('path');
+var spawn = require('child_process').spawn;
+
+function toStdOut(data) {
+	process.stdout.write(data.toString());
+}
+
+function toStdErr(data) {
+	process.stderr.write(data.toString());
+}
 
 module.exports = function(opts) {
 	var test = opts.test;
 	var env = opts.env || 'ie10,firefox36,chrome41,iphone7,Android_Nexus7HD';
 	var config = opts.config || path.join(__dirname, '..', 'config', 'nightwatch.json');
-	return exec('nightwatch'
-		+ ' --env ' + env
-		+ ' --test ' + test
-		+ ' --config ' + config);
+
+	return new Promise(function(resolve, reject) {
+		var nightwatch = spawn('nightwatch', [
+			'--env ' + env,
+			'--test ' + test,
+			'--config ' + config
+		], { cwd: process.cwd(), env: env });
+		nightwatch.stdout.on('data', toStdOut);
+		nightwatch.stderr.on('data', toStdErr);
+		nightwatch.on('error', reject);
+		nightwatch.on('close', resolve);
+	});
 };
