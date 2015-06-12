52c5ffcfe6702c987e324cfc14c1ea6405cf4e1e
Matthew Andrews <matt@mattandre.ws>
2015-06-12 16:27:00 +0100

    What is happening…

--- tasks/nightwatch.js
+++ tasks/nightwatch.js
@@ -15,13 +15,15 @@ module.exports = function(opts) {
 	var test = opts.test;
 	var env = opts.env || 'ie10,firefox36,chrome41,iphone7,Android_Nexus7HD';
 	var config = opts.config || path.join(__dirname, '..', 'config', 'nightwatch.json');
+	var args = [
+		'--env "' + env + '"',
+		'--test "' + test + '"',
+		'--config "' + config + '"'
+	];
+	console.log(args);
 
 	return new Promise(function(resolve, reject) {
-		var nightwatch = spawn('nightwatch', [
-			'--env ' + env,
-			'--test ' + test,
-			'--config ' + config
-		], { cwd: process.cwd() });
+		var nightwatch = spawn('nightwatch', args, { cwd: process.cwd() });
 		nightwatch.stdout.on('data', toStdOut);
 		nightwatch.stderr.on('data', toStdErr);
 		nightwatch.on('error', reject);
