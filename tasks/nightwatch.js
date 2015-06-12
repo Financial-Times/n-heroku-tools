91c8cb3c04b6ac7278391269f1dac8ab85e2411f
Matthew Andrews <matt@mattandre.ws>
2015-06-12 16:40:10 +0100

    spawn is crazy

--- tasks/nightwatch.js
+++ tasks/nightwatch.js
@@ -15,12 +15,7 @@ module.exports = function(opts) {
 	var test = opts.test;
 	var env = opts.env || 'ie10,firefox36,chrome41,iphone7,Android_Nexus7HD';
 	var config = opts.config || path.join(__dirname, '..', 'config', 'nightwatch.json');
-	var args = [
-		'--env "' + env + '"',
-		'--test "' + test + '"',
-		'--config "' + config + '"'
-	];
-	console.log(args);
+	var args = [ '--env', env, '--test', test, '--config', config ];
 
 	return new Promise(function(resolve, reject) {
 		var nightwatch = spawn('nightwatch', args, { cwd: process.cwd() });
