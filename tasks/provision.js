8e46dd28075d0d3ff32b52ae095f31d91eb96858
Matthew Andrews <matt@mattandre.ws>
2015-08-22 11:37:58 +0100

    FT Org

--- tasks/provision.js
+++ tasks/provision.js
@@ -3,5 +3,5 @@
 var spawn = require('../lib/spawn');
 
 module.exports = function(name) {
-	return spawn(['heroku', 'create', '-a', name, '--region', 'us'], { verbose: true });
+	return spawn(['heroku', 'create', '-a', name, '--region', 'us', '--org', 'financial-times'], { verbose: true });
 };
