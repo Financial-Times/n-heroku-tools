43ddd883cee27e8cbc95a5f178a838ca8a65b966
Rhys Evans <rhys.evans@ft.com>
2015-08-27 16:34:38 +0100

    test-urls now fails when expected

--- tasks/test-urls.js
+++ tasks/test-urls.js
@@ -2,9 +2,26 @@
 require('array.prototype.find');
 var path = require('path');
 var normalizeName = require('../lib/normalize-name');
-var Directly = require('directly');
+var directly = require('directly');
 var packageJson = require(process.cwd() + '/package.json');
 var appName;
+
+var getExpectation = function (expected) {
+
+	if (typeof expected === 'string') {
+		expected = {
+			redirect: expected
+		};
+	} else {
+		if (typeof expected === 'number') {
+			expected = {
+				status: expected
+			};
+		}
+		expected.status = expected.status || 200;
+	}
+	return expected;
+};
 
 function testUrls (opts) {
 
@@ -12,116 +29,104 @@ function testUrls (opts) {
 	var headers = opts.headers || {};
 
 	var fetchers = Object.keys(opts.urls).map(function (url) {
-		var expected = opts.urls[url];
+		return function () {
+			var expected = getExpectation(opts.urls[url]);
 
-		if (typeof expected === 'string') {
-			expected = {
-				redirect: expected
-			};
-		} else {
-			if (typeof expected === 'number') {
-				expected = {
-					status: expected
-				};
-			}
-			expected.status = expected.status || 200;
-		}
+			return new Promise(function(resolve, reject) {
 
-		return new Promise(function(resolve, reject) {
+				function end(message) {
+					console.log(message);
+					clearTimeout(timeout);
+					clearInterval(checker);
+					resolve();
+				}
 
-			function end(message) {
-				console.log(message);
-				clearTimeout(timeout);
-				clearInterval(checker);
-				resolve();
-			}
-			var timeout;
-			var checker;
-			var failures = [];
-			function checkGtg() {
-				console.log('polling:' + baseUrl + url);
-				fetch(baseUrl + url, {
-						timeout: opts.timeout || 2000,
-						headers: headers
-					})
-					.then(function(response) {
-						return new Promise(function (resolve, reject) {
-							if (expected.status) {
-								if (response.status !== expected.status) {
-									if (failures.indexOf('bad status: ' + response.status) === -1) {
-										failures.push('bad status: ' + response.status);
+				var timeout;
+				var checker;
+				var failures = [];
+
+				function checkGtg() {
+					console.log('polling:' + baseUrl + url);
+					fetch(baseUrl + url, {
+							timeout: opts.timeout || 2000,
+							headers: headers
+						})
+						.then(function(response) {
+							return new Promise(function (resolve, reject) {
+								if (expected.status) {
+									if (response.status !== expected.status) {
+										if (failures.indexOf('bad status: ' + response.status) === -1) {
+											failures.push('bad status: ' + response.status);
+										}
+										return reject();
 									}
-									return reject();
 								}
-							}
 
-							if (expected.redirect) {
-								var arrivedAt = response.url.replace(baseUrl, '');
-								if (arrivedAt !== expected.redirect) {
-									if (failures.indexOf('bad redirect: ' + arrivedAt) === -1) {
-										failures.push('bad redirect: ' + arrivedAt);
+								if (expected.redirect) {
+									var arrivedAt = response.url.replace(baseUrl, '');
+									if (arrivedAt !== expected.redirect) {
+										if (failures.indexOf('bad redirect: ' + arrivedAt) === -1) {
+											failures.push('bad redirect: ' + arrivedAt);
+										}
+										return reject();
 									}
-									return reject();
 								}
-							}
 
-							if (expected.headers) {
-								var badHeaders = Object.keys(expected.headers).filter(function (key) {
-									return response.headers.get(key) !== expected.headers[key];
+								if (expected.headers) {
+									var badHeaders = Object.keys(expected.headers).filter(function (key) {
+										return response.headers.get(key) !== expected.headers[key];
+									});
+
+									if (badHeaders.length) {
+										badHeaders = badHeaders.map(function (header) {
+											return header + ':' + response.headers.get(header);
+										}).join('; ');
+
+										if (failures.indexOf('bad headers: ' + badHeaders) === -1) {
+											failures.push('bad headers: ' + badHeaders);
+										}
+										return reject();
+									}
+								}
+
+								if (expected.content) {
+									return response
+										.text()
+										.then(function (text) {
+											if (text !== expected.content) {
+												if (failures.indexOf('bad content: ' + text) === -1) {
+													failures.push('bad content: ' + text);
+												}
+												reject();
+											} else {
+												resolve();
+											}
+										});
+								}
+
+								resolve();
+							})
+								.then(function () {
+									end(baseUrl + url + ' responded as expected');
 								});
 
-								if (badHeaders.length) {
-									badHeaders = badHeaders.map(function (header) {
-										return header + ':' + response.headers.get(header);
-									}).join('; ');
-
-									if (failures.indexOf('bad headers: ' + badHeaders) === -1) {
-										failures.push('bad headers: ' + badHeaders);
-									}
-									return reject();
-								}
-							}
-
-							if (expected.content) {
-								return response
-									.text()
-									.then(function (text) {
-										if (text !== expected.content) {
-											if (failures.indexOf('bad content: ' + text) === -1) {
-												failures.push('bad content: ' + text);
-											}
-											reject();
-										} else {
-											resolve();
-										}
-									});
-							}
-
-							resolve();
 						})
-							.then(function () {
-								end(baseUrl + url + ' responded as expected');
-							});
-
-					})
-					.catch(function(err) {
-						if (err.message.indexOf('timeout') > -1 ) {
-							failures.push('endpoint too slow');
-						}
-					});
-			}
-			checker = setInterval(checkGtg, 3000);
-			timeout = setTimeout(function() {
-				console.log(baseUrl + url + ' keeps failing with: ' + failures.join(', '));
-				reject('Test url polling failed');
-				clearInterval(checker);
-			}, 2*60*1000);
-		});
-
-
+						.catch(function(err) {
+							if (err.message.indexOf('timeout') > -1 ) {
+								failures.push('endpoint too slow');
+							}
+						});
+				}
+				checker = setInterval(checkGtg, 3000);
+				timeout = setTimeout(function() {
+					console.log(baseUrl + url + ' keeps failing with: ' + failures.join(', '));
+					reject('Test url polling failed');
+					clearInterval(checker);
+				}, 2*60*1000);
+			});
+		};
 	});
-
-	return new Directly(10, fetchers);
+	return directly(10, fetchers);
 
 }
 
