9acd065460da26da43f349ce76eb57309b7e7cef
Matthew Andrews <matt@mattandre.ws>
2015-10-23 16:41:26 +0100

    Remove splunk drain set up from NBT — in the future Splunk integration will be done via ft-next-logger

--- tasks/ship.js
+++ tasks/ship.js
@@ -21,7 +21,6 @@ module.exports = function ship(opts){
 		let pipelineName = opts.pipeline || packageJson.name;
 		log.info('Deploy to ' + pipelineName);
 		let apps = yield pipelines.getApps(pipelineName);
-		let splunkDrain = 'splunk' in opts ? opts.splunk : true;
 		if(!apps.staging){
 			log.error('No staging app found');
 			return;
@@ -41,15 +40,15 @@ module.exports = function ship(opts){
 
 		log.info('Found apps %j', apps);
 
-		if(opts.configure){
+		if (opts.configure) {
 			log.log('Configure enabled');
 			let source = pipelineName;
 			let configureTasks = [
-				configure({source:source, target:apps.staging, splunk:splunkDrain}),
-				configure({source:source, target:apps.production.eu, overrides:['REGION=EU'], splunk:splunkDrain})
+				configure({ source: source, target: apps.staging }),
+				configure({ source: source, target: apps.production.eu, overrides: ['REGION=EU'] })
 			];
-			if(opts.multiregion){
-				configureTasks.push(configure({source:source, target:apps.production.us, overrides:['REGION=US'], splunk:splunkDrain}))
+			if (opts.multiregion) {
+				configureTasks.push(configure({ source: source, target: apps.production.us, overrides: ['REGION=US'] }))
 			}
 
 			log.log('Configure all apps');
