47a4850b5ef7c72bf02d07dacae04179300faf40
Laura Carvajal <laura.carvajal@gmail.com>
2016-04-19 09:45:46 +0100

    pass retries and suiteRetries options through if present

--- tasks/nightwatch.js
+++ tasks/nightwatch.js
@@ -16,7 +16,17 @@ function task (opts) {
 	var test = opts.test;
 	var env = opts.env || 'ie9,ie10,ie11,firefox38,firefox39,chrome42,chrome43,iphone6_plus,Android_Nexus7HD';
 	var config = opts.config || path.join(__dirname, '..', 'config', 'nightwatch.json');
+
 	var args = [ '--env', env, '--test', test, '--config', config ];
+
+	if (opts.retries) {
+		args.push('--retries');
+		args.push(opts.retries);
+	}
+	if (opts.suiteRetries) {
+		args.push('--suiteRetries');
+		args.push(opts.suiteRetries);
+	}
 
 	return new Promise(function (resolve, reject) {
 		var nightwatch = spawn('nightwatch', args, { cwd: process.cwd() });
@@ -38,6 +48,8 @@ module.exports = function (program, utils) {
 		.command('nightwatch [test]')
 		.option('-c, --config <config>', 'The location of the nightwatch.json, defaults to Next Build Tools nightwatch.json')
 		.option('-e, --env <env>', 'The location of the nightwatch.json, defaults to Next Build Tools defined environments')
+		.option('--retries <retries>', 'Retries failed or errored testcases up to the specified number of times')
+		.option('--suiteRetries <suiteRetries>', 'Retries failed or errored testsuites up to the specified number of times')
 		.description('runs nightwatch with some sensible defaults')
 		.action(function (test, options) {
 			task({
