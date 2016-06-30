124dafae2a77be98bf52aaeca08b3c324282817b
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-06-30 11:38:59 +0100

    Tidy method

--- config/nightwatch-globals.js
+++ config/nightwatch-globals.js
@@ -17,18 +17,17 @@ module.exports = {
 	afterEach: function (browser, done) {
 		const sessionId = browser.sessionId;
 		const results = browser.currentTest.results;
-		const failed = results.failed || results.errors;
 		console.log(`Sauce Test Results at https://saucelabs.com/tests/${browser.sessionId}`);
-		if (failed) {
+		if (results.failed || results.errors) {
 			browser
 				.getLog('browser', logs => {
 					console.log('JS Console');
 					console.log('==========');
 					console.log(logs);
 				})
-				.end(() => notify(sessionId, !failed).then(done));
+				.end(() => notify(sessionId, false).then(done));
 		} else {
-			notify(sessionId, !failed)
+			notify(sessionId, true)
 				.then(err => {
 					browser.end();
 					done(err);
