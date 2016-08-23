a5e40549da2dbd8c71017c3dbd5fddfa7228f1d8
Rhys Evans <rhys.evans@ft.com>
2016-08-23 11:17:22 +0100

    add expectations for cache headers

--- tasks/smoke.js
+++ tasks/smoke.js
@@ -5,9 +5,11 @@ const task = opts => smokeTest.run(opts);
 module.exports = function (program, utils) {
 	program
 		.command('smoke [app]')
+		.option('--auth', 'Authenticate with FT_NEXT_BACKEND_KEY')
 		.description('Tests that a given set of urls for an app respond as expected. Expects the config file ./test/smoke.js to exist')
-		.action(function (app) {
+		.action(function (app, opts) {
 			task({
+				authenticated: opts.auth,
 				app: app
 			}).catch(utils.exit);
 		});
