d4612f026a0e808e4b5bf2903e56ec7f0a33c8e3
Laura Carvajal <laura.carvajal@gmail.com>
2016-09-20 12:31:16 +0100

    spawn

--- tasks/a11y.js
+++ tasks/a11y.js
@@ -1,10 +1,22 @@
 'use strict';
 
+var spawn = require('child_process').spawn;
+
+function task (options) {
+	var app = options.app;
+	var promise = Promise.resolve();
+
+	promise = promise.then(function () {
+		return spawn('pa11y-ci', { verbose: true });
+	});
+	return promise;
+};
+
 
 module.exports = function (program, utils) {
 	program
 		.command('pa11y-ci [app]')
-		.action(function () {
+		.action(function (app) {
 			task({
 				app: app
 			})
