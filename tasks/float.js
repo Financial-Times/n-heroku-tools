37d4af179bd28092578fc731d2058ef860bfbe37
Keith Rogers <git@keithrogers.eu>
2017-02-08 17:03:23 +0000

    Stop referencing NBT unless referring to old versions
    Avoiding the tempting yak shave of major bumping and removing bits 😬

--- tasks/float.js
+++ tasks/float.js
@@ -68,7 +68,7 @@ module.exports = function (program, utils) {
 		.description('Deploys code to a test app and checks it doesn\'t die')
 		.option('-a --app', 'Name of the app')
 		.option('-t --testapp [value]', 'Name of the app to be created')
-		.option('-m --master', "Run even if on master branch (not required if using nbt ship).")
+		.option('-m --master', "Run even if on master branch (not required if using nht ship).")
 		.option('-d, --no-destroy', 'Don\'t automatically destroy new apps')
 		.option('-s, --skip-gtg', 'skip the good-to-go HTTP check')
 		.action(function (options){
