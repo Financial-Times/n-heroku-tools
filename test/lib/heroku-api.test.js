01c1f1f4513bcdd5994b657adb43bc7800a7d820
Tak Tran <contact@tutaktran.com>
2018-11-08 16:13:40 +0000

    Clean up heroku api test using nockScope 🐿 v2.10.3

--- test/lib/heroku-api.test.js
+++ test/lib/heroku-api.test.js
@@ -3,10 +3,18 @@ const expect = require('chai').expect;
 const herokuApi = require('../../lib/heroku-api');
 
 describe('heroku-api', function () {
+	let nockScope;
+	before(() => {
+		nockScope = nock('https://api.heroku.com');
+	});
+
+	afterEach(() => {
+		nock.cleanAll();
+	});
+
 	it('calls the endpoint', async () => {
 		const endpoint = '/something';
-		nock('https://api.heroku.com')
-			.get(endpoint)
+		nockScope.get(endpoint)
 			.reply(200, {
 				something: 'yes'
 			});
@@ -20,8 +28,7 @@ describe('heroku-api', function () {
 	it('uses the auth token', async () => {
 		const endpoint = '/';
 		const authToken = 'some123';
-		nock('https://api.heroku.com')
-			.get(endpoint)
+		nockScope.get(endpoint)
 			.reply(200, function () {
 				const { headers } = this.req;
 				return {
@@ -36,8 +43,7 @@ describe('heroku-api', function () {
 	it('throws an error on failure', async () => {
 		const endpoint = '/';
 		const authToken = 'some123';
-		nock('https://api.heroku.com')
-			.get(endpoint)
+		nockScope.get(endpoint)
 			.reply(400, {});
 
 		await herokuApi({ endpoint, authToken })
