225fc1bac437dce61928f41b60b06291edf50a4b
Tak Tran <contact@tutaktran.com>
2019-02-06 13:00:21 +0000

    Add integration test for nht configure 🐿 v2.10.3

---
+++ tasks/configure.test.js
@@ -0,0 +1,132 @@
+require('isomorphic-fetch');
+const nock = require('nock');
+
+const configure = require('./configure');
+const createMockProgram = require('./__mocks__/create-mock-program');
+
+const REGISTRY_URL = 'https://some-registry.com';
+
+const mockConfigureEndpoints = ({
+	herokuScope,
+	registryScope,
+	vaultScope,
+	source,
+	target,
+	pipelineId,
+	systemCode,
+	registryName
+}) => {
+	// Get pipelines information
+	const endpoint = `/pipelines/${source}`;
+	herokuScope.get(endpoint).reply(200, () => {
+		return {
+			id: pipelineId
+		};
+	});
+
+	// Get registry info for vault location
+	registryScope.get('/').reply(200, () => {
+		return [
+			{
+				name: registryName,
+				code: systemCode,
+				config: `https://vault.in.ft.com/v1/secret/teams/next/${systemCode}`
+			}
+		];
+	});
+
+	// Get vault vars
+	vaultScope
+		.get('/v1')
+		.reply(200, () => {
+			return {};
+		})
+		.get('/v1/secret/teams/next/shared/production')
+		.reply(200, () => {
+			return {};
+		})
+		.get(`/v1/secret/teams/next/${systemCode}/production`)
+		.reply(200, () => {
+			return {};
+		})
+		.get(`/v1/secret/teams/next/${systemCode}/shared`)
+		.reply(200, () => {
+			return {
+				data: {
+					env: []
+				}
+			};
+		});
+
+	// Get environment variables from heroku
+	herokuScope
+		.get(`/apps/${target}/config-vars`)
+		.reply(200, () => {
+			return {};
+		})
+		.patch(`/apps/${target}/config-vars`)
+		.reply(200, () => {
+			return {};
+		});
+};
+
+describe('configure', () => {
+	let mockProgram;
+	let herokuScope;
+	let registryScope;
+	let vaultScope;
+	beforeAll(() => {
+		herokuScope = nock('https://api.heroku.com');
+		registryScope = nock(REGISTRY_URL);
+		vaultScope = nock('https://vault.in.ft.com');
+	});
+
+	beforeEach(() => {
+		mockProgram = createMockProgram();
+		configure(mockProgram, {
+			list: () => {},
+			exit: () => {}
+		});
+	});
+
+	afterEach(() => {
+		jest.resetAllMocks();
+		nock.cleanAll();
+	});
+
+	afterAll(() => {
+		jest.unmock('shellpromise');
+	});
+
+	it('shows help', () => {
+		const [firstCall] = mockProgram.command.mock.calls;
+		const [commandFn] = firstCall;
+		expect(commandFn).toContain('configure');
+	});
+
+	it('run action', async () => {
+		const [firstCall] = mockProgram.action.mock.calls;
+		const [actionFn] = firstCall;
+
+		const source = 'ft-next-hello';
+		const target = 'ft-next-hello-staging';
+		const pipelineId = 'ft-next-hello-id';
+		const systemCode = 'next-hello';
+		const registryName = 'hello';
+
+		mockConfigureEndpoints({
+			herokuScope,
+			registryScope,
+			vaultScope,
+			source,
+			target,
+			pipelineId,
+			systemCode,
+			registryName
+		});
+
+		await actionFn(source, target, {
+			registry: REGISTRY_URL
+		});
+	});
+});
