88064b4fee625b0f60db88d28ad35d6d90ad7795
Matt Hinchliffe <matt.hinchliffe@ft.com>
2019-07-04 13:59:21 +0100

    Add --manifest-file option to deploy hashed assets task to enable configuration of the manifest filename  🐿 v2.12.4

--- tasks/deploy-hashed-assets.js
+++ tasks/deploy-hashed-assets.js
@@ -11,6 +11,7 @@ const aws = require('aws-sdk');
 const AWS_ACCESS_HASHED_ASSETS = process.env.AWS_ACCESS_HASHED_ASSETS || process.env.aws_access_hashed_assets;
 const AWS_SECRET_HASHED_ASSETS = process.env.AWS_SECRET_HASHED_ASSETS || process.env.aws_secret_hashed_assets;
 
+const manifestFile = 'asset-hashes.json';
 const euBucket = 'ft-next-hashed-assets-prod';
 const usBucket = 'ft-next-hashed-assets-prod-us';
 const euRegion = 'eu-west-1';
@@ -48,10 +49,9 @@ function task (opts) {
 	let assetHashes;
 
 	try {
-		console.log(process.cwd() + `/${directory}/asset-hashes.json`); // eslint-disable-line no-console
-		assetHashes = require(process.cwd() + `/${directory}/asset-hashes.json`);
+		assetHashes = require(process.cwd() + `/${directory}/${opts.manifestFile}`);
 	} catch(err) {
-		return Promise.reject('Must run `make build-production` before running `nbt deploy-hashed-assets`');
+		return Promise.reject('Failed to load hashed assets manifest file.');
 	}
 
 	if (!(AWS_ACCESS_HASHED_ASSETS && AWS_SECRET_HASHED_ASSETS)) {
@@ -124,6 +124,7 @@ module.exports = function (program, utils) {
 	program
 		.command('deploy-hashed-assets')
 		.description('deploys hashed asset files to S3 (if AWS keys set correctly)')
+		.option('--manifest-file <filename>', 'Name of the manifest file to read', manifestFile)
 		.option('--directory <directory>', 'Directory of assets to deploy (defaults to public)')
 		.option('--namespace <namespace>', 'Name of the folder to upload into (defaults to the app name in package.json')
 		.option('--monitor-assets', 'Send asset sizes to Graphite')
@@ -132,6 +133,7 @@ module.exports = function (program, utils) {
 		.action(function (options) {
 			task(options).catch(utils.exit);
 		});
+
 };
 
 module.exports.task = task;
